5ce23f5acc268db4431654de4ae847c4f7867372
Ian2570 <138541510+Ian2570@users.noreply.github.com>
2026-07-31 16:25:25 -0300

    Update models.py

--- contact/models.py
+++ contact/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils import timezone
 
+
+#model
 class Contact(models.Model): 
     first_name = models.CharField(max_lenght=50)
     last_name = models.CharField(max_length=50)
