f521cd020942caf6021c3f2a510943f6ee811b06
Ian2570 <138541510+Ian2570@users.noreply.github.com>
2026-07-31 16:16:17 -0300

    Update models.py

--- contact/models.py
+++ contact/models.py
@@ -2,8 +2,9 @@ from django.db import models
 from django.utils import timezone
 
 class Contact(models.Model): 
-    first_name = models.CharField(maxlenght=50)
+    first_name = models.CharField(max_lenght=50)
     last_name = models.CharField(max_length=50)
     phone = models.CharField(max_length=50)
     email = models.EmailField(max_length= 254)
     created_date = models.DateTimeField(default= timezone.now)
+    description = models.TextField()
