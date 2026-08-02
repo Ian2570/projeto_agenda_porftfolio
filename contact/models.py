7efda0ca1cd5cbe8ad05633db6fb4b7fe45d0082
Ian2570 <138541510+Ian2570@users.noreply.github.com>
2026-08-02 11:14:54 -0300

    Atualizção Meta/Owner

--- contact/models.py
+++ contact/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # id (primary key - automático)
 # first_name (string), last_name (string), phone (string)
@@ -9,6 +10,10 @@ from django.utils import timezone
 # category (foreign key), show (boolean), owner (foreign key)
 # picture (imagem)
 class Category(models.Model):
+    class Meta:
+         verbose_name: 'Category'
+
+
     name = models.CharField(max_length=50)
 
     def __str__(self) -> str:
@@ -24,6 +29,8 @@ class Contact(models.Model):
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='picture/%Y/%m')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL,
+                                 blank=True, null=True)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL,
                                  blank=True, null=True)
 
     def __str__(self) -> str:
