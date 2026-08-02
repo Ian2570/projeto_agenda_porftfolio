ffd0b46818e551d4b498fc768ac420435f8e07db
Ian2570 <138541510+Ian2570@users.noreply.github.com>
2026-08-02 07:51:05 -0300

    Criação de seeds de teste

--- contact/admin.py
+++ contact/admin.py
@@ -6,3 +6,5 @@ from contact import models
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
     pass
+list_display = 'first_name', 'last_name', 'phone',
+ordering = 'id', 'first_name', 'last_name',
